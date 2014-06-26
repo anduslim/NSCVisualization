a8fb0e65104db5b6c954469835e838670532b7df
gauravmm <gaurav@manek.sg>
2014-06-26 15:01:05 +0800

    Serial Read test

--- serialToMySQL.py
+++ serialToMySQL.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
 
-import serial
+import serial, os, time
 
 def main(sport, baud):
 	s = serial.Serial(sport, baud)
+	currStr = b""
+	isRecording = False
 	while s.isOpen():
 		inp = s.read()
-		print("{}\t{}\n".format(inp, ord(inp)))
+		if ord(inp) == 6:
+			isRecording = True
+			currStr = b""
+		elif ord(inp) == 0:
+			if(isRecording):
+				if(len(currStr) > 3): # Handle a new line here!
+					currStr = currStr.decode("utf-8").split(",")
+					if len(currStr) == 4:
+						# This is bad:
+						INSERT_STRING="INSERT INTO demo VALUES ({}, {}, {}, {}, {})".format(time.time(), currStr[3], currStr[0], currStr[1], currStr[2])
+						# I am ashamed that I can even write such code.
+						os.system("mysql -u user sensor_readings -e \"{}\"".format(INSERT_STRING))
+						# At least I'm not putting my name to it.
+					else:
+						print("Unexpected Input: {}".format(str(currStr)))
+			isRecording = False
+		else:
+			currStr += inp
 	
 
 if __name__ == "__main__" :
